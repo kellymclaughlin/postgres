053d3e0c6884ef24b8f58af3565f770787d0ff10
Jerry Jelinek <jerry.jelinek@joyent.com>
2018-07-30 16:42:25 +0000

    MANATEE-401 postgresql should not reuse old WAL files Reviewed by: Joshua M. Clulow <jmc@joyent.com> Approved by: Joshua M. Clulow <jmc@joyent.com>

--- src/include/access/xlog.h
+++ src/include/access/xlog.h
@@ -103,6 +103,7 @@ extern int	wal_retrieve_retry_interval;
 extern char *XLogArchiveCommand;
 extern bool EnableHotStandby;
 extern bool fullPageWrites;
+extern bool	wal_recycle;
 extern bool wal_log_hints;
 extern bool wal_compression;
 extern bool *wal_consistency_checking;
